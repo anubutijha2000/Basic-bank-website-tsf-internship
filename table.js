7b6d2cc8404996ba8713d7a4692522136fb933b3
Anubhuti Jha <60725177+anubutijha2000@users.noreply.github.com>
2021-03-21 22:01:06 +0530

    Add files via upload

---
+++ table.js
@@ -0,0 +1,21 @@
+class user_detail{
+    constructor(emailid,name,amount) {
+        this.name= name;
+        this.amount= amount;
+        this.emailid= emailid;
+    }
+}
+
+ 
+    var udetail= new Array();
+     udetail.push(new user_detail("anubhutijha@gmail.com", "Anubhuti Jha", 3000));
+     udetail.push(new user_detail("amisha.soni@gmail.com", "Amisha Soni" , 1000));
+     udetail.push(new user_detail("bhavya2000soni@gmail.com", "Bhavya Soni" , 2500));
+     udetail.push(new user_detail("garima2@gmail.com", "Garima Soni" , 500));
+     udetail.push(new user_detail("himanshipal@gmail.com", "Himanshi Paliwal" , 100));
+     udetail.push(new user_detail("ishansharma@gmail.com", "Ishan Sharma" , 1700));
+     udetail.push(new user_detail("anshjain@gmail.com", "Ansh Jain" , 2000));
+     udetail.push(new user_detail("kshitizsharma@gmail.com", "Kshitiz Sharma" , 2200));
+     udetail.push(new user_detail( "harshsaxena@gmail.com", "Harsh Saxena" ,2300));
+     udetail.push(new user_detail("abdulsaini@gmail.com", "Abdul Saini" , 1300));
+
